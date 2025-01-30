2c89a1b4afeb7e744d3414817115aa4ca48127b9
Ingan121 <lssaa91192@gmail.com>
2025-01-30 15:57:59 +0900

    Make shell scripts posix compliant

--- installer/install.sh
+++ installer/install.sh
@@ -1,34 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 # Function to print error messages
-function print_error {
+print_error() {
     echo "$1" >&2
 }
 
 # Function to download the latest versions information
-function get_latest_versions {
+get_latest_versions() {
     echo "Getting the latest versions of WMPotify and WMPVis..."
-    local latest_versions_url="https://www.ingan121.com/wmpotify/latest.txt"
-    local latest_versions=$(curl -s "$latest_versions_url")
-    if [[ -z "$latest_versions" ]]; then
+    latest_versions_url="https://www.ingan121.com/wmpotify/latest.txt"
+    latest_versions=$(curl -s "$latest_versions_url")
+    if [ -z "$latest_versions" ]; then
         print_error "Failed to download the latest versions information."
         exit 1
     fi
 
-    local theme_version=$(echo "$latest_versions" | grep -oP 'wmpotify\s*=\s*\K.*')
-    local vis_version=$(echo "$latest_versions" | grep -oP 'wmpvis\s*=\s*\K.*')
+    theme_version=$(echo "$latest_versions" | grep 'wmpotify\s*=' | awk -F '=' '{print $2}' | tr -d ' ')
+    vis_version=$(echo "$latest_versions" | grep 'wmpvis\s*=' | awk -F '=' '{print $2}' | tr -d ' ')
 
     echo "Latest versions - WMPotify: $theme_version, WMPVis: $vis_version"
     echo "$theme_version" "$vis_version"
 }
 
 # Function to download and extract a zip file
-function download_and_extract {
-    local url="$1"
-    local dest_dir="$2"
-    local temp_zip=$(mktemp)
+download_and_extract() {
+    url="$1"
+    dest_dir="$2"
+    temp_zip=$(mktemp)
 
     echo "Downloading $url..."
     curl -L -o "$temp_zip" "$url"
@@ -38,7 +38,7 @@ function download_and_extract {
     rm "$temp_zip"
 }
 
-function print_hello {
+print_hello() {
     echo "WMPotify for Spicetify Installer"
     echo "Made by Ingan121"
     echo
@@ -48,31 +48,33 @@ function print_hello {
 }
 
 # Main script
-function main {
+main() {
     print_hello
 
-    if ! command -v spicetify &> /dev/null; then
+    if ! command -v spicetify >/dev/null 2>&1; then
         echo "spicetify could not be found. Please install spicetify first."
         exit 1
     fi
 
-    local spicetify_path=="$(dirname "$(spicetify -c)")"
-    local theme_path="$spicetify_path/Themes/WMPotify"
-    local vis_app_path="$spicetify_path/CustomApps/wmpvis"
+    spicetify_path=$(dirname "$(spicetify -c)")
+    theme_path="$spicetify_path/Themes/WMPotify"
+    vis_app_path="$spicetify_path/CustomApps/wmpvis"
 
     # Get the latest versions
-    read -r theme_version vis_version < <(get_latest_versions)
+    set -- $(get_latest_versions)
+    theme_version=$1
+    vis_version=$2
 
-    if [[ -n "$SKIP_THEME" ]]; then
-        echo "Skipping theme installation as SkipTheme is set."
+    if [ -n "$SKIP_THEME" ]; then
+        echo "Skipping theme installation as SKIP_THEME is set."
     else
         # Download and install WMPotify theme
-        local theme_url="https://github.com/Ingan121/WMPotify/releases/download/$theme_version/WMPotify-$theme_version.zip"
+        theme_url="https://github.com/Ingan121/WMPotify/releases/download/$theme_version/WMPotify-$theme_version.zip"
         download_and_extract "$theme_url" "$theme_path"
     fi
 
     # Download and install WMPotify NowPlaying app
-    local vis_url="https://github.com/Ingan121/WMPotify/releases/download/$vis_version/WMPotify-NowPlaying-$vis_version.zip"
+    vis_url="https://github.com/Ingan121/WMPotify/releases/download/$vis_version/WMPotify-NowPlaying-$vis_version.zip"
     download_and_extract "$vis_url" "$vis_app_path"
 
     spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
--- installer/uninstall.sh
+++ installer/uninstall.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+
 echo "Uninstalling WMPotify and WMPVis..."
 
-if ! command -v spicetify &> /dev/null; then
+if ! command -v spicetify >/dev/null 2>&1; then
     echo "spicetify could not be found. Exiting..."
     exit 1
 fi
 
-spicetify_path="$(dirname "$(spicetify -c)")"
+spicetify_path=$(dirname "$(spicetify -c)")
 theme_path="$spicetify_path/Themes/WMPotify"
 vis_app_path="$spicetify_path/CustomApps/wmpvis"
 
@@ -15,10 +17,10 @@ current_apps=$(echo "$current_apps" | grep -v "wmpvis")
 if echo "$current_apps" | grep -q "marketplace"; then
     spicetify config current_theme marketplace
 else
-    spicetify config current_theme " "
+    spicetify config current_theme ""
 fi
 
-spicetify config color_scheme " "
+spicetify config color_scheme ""
 spicetify config custom_apps ""
 spicetify config custom_apps "$(echo "$current_apps" | tr '\n' '|')"
 
